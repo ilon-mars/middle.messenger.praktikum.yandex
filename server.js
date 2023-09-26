3acbe34d1fde31bb74a8a2bf0a3c9f10647d2b29
Maryana <ilon.mars@yandex.ru>
2023-09-26 22:52:22 +0300

    feat: express server

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(`${__dirname}/dist`));
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`);
+});
