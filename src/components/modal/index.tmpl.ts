27494318a21bf479f1c42199610e8c6acf20774d
Maryana <ilon.mars@yandex.ru>
2023-10-12 21:20:24 +0300

    refactor: move modals to own files

--- src/components/modal/index.tmpl.ts
+++ src/components/modal/index.tmpl.ts
@@ -1,14 +1,4 @@
-export const loginTmpl = `
-  <h2 class="h2 {{ $style.title }}" id="title">{{ title }}</h2>
-
-  {{{form}}}
-
-  <footer class="{{ $style.controls }}">
-    {{{ link }}}
-  </footer>
-`;
-
-export const registerTmpl = `
+export const tmpl = `
   <h2 class="h2 {{ $style.title }}">{{ title }}</h2>
 
   {{{form}}}
